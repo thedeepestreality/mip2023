c09929932bcce074e5d78c5ac2dfbf73ed3e524e
thedeepestreality <zetoster@gmail.com>
2023-02-27 16:53:23 +0300

    added numeric integration of the pendulum ode

--- pendulum.py
+++ pendulum.py
@@ -2,8 +2,10 @@ import pybullet as p
 import time
 import pybullet_data
 import numpy as np
+from scipy.integrate import odeint
+import math
 
-GUI = True
+GUI = False
 
 if (GUI):
     physicsClient = p.connect(p.GUI)
@@ -22,8 +24,11 @@ if (GUI):
 else:
     physicsClient = p.connect(p.DIRECT)
 
-p.setGravity(0, 0, -10)
+g = 10
+L = 0.5
+p.setGravity(0, 0, -g)
 bodyId = p.loadURDF("./pendulum.urdf")
+
 
 q0 = 0.5 # starting position
 dt = 1/240 # pybullet simulation step
@@ -62,7 +67,31 @@ for t in logTime[1:]:
     
 p.disconnect()
 
+def rp(x, t):
+    return [x[1], -g/L*math.sin(x[0])]
+
+theta = odeint(rp, [q0, 0], logTime)
+logTheta = theta[:,0]
+
+def cost(q_exp, q_theor):
+    l2 = 0
+    sz = len(q_exp)
+    linf = abs(q_exp[0] - q_theor[0])
+    for i in range(sz):
+        err = abs(q_exp[i] - q_theor[i])
+        if (err > linf):
+            linf = err
+        l2 += err**2
+    l2 = math.sqrt(l2)
+    return (l2, linf)
+
+(l2, linf) = cost(logPos, logTheta)
+print(f'l2 odeint = {l2}')
+print(f'linf odeint = {linf}')
+
 import matplotlib.pyplot as plt
-plt.plot(logTime, logPos)
+plt.plot(logTime, logPos, label = "sim")
 plt.grid(True)
+plt.plot(logTime, logTheta, label = "theor")
+plt.legend()
 plt.show()
